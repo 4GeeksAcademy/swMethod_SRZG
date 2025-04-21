26f0ca7671003e2321bbf50f5420ddc326ea7d49
AlleriaDev <142839879+AlleriaDev@users.noreply.github.com>
2025-04-21 12:29:42 +0200

    Method Added

--- src/models.py
+++ src/models.py
@@ -1,19 +1,86 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(40), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(40), nullable=False)
+    full_name: Mapped[str] = mapped_column(String(200), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(100), nullable=False, unique=True)
+
+    favorites = relationship('Favorites', backref='user', lazy=True)
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Planets(db.Model):
+    __tablename__ = "planet"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    diameter: Mapped[str] = mapped_column(String(100), nullable=False)
+    rotation_period: Mapped[str] = mapped_column(String(100), nullable=False)
+    orbital_period: Mapped[str] = mapped_column(String(100), nullable=False)
+    gravity: Mapped[str] = mapped_column(String(100), nullable=False)
+    population: Mapped[str] = mapped_column(String(100), nullable=False)
+    climate: Mapped[str] = mapped_column(String(100), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(100), nullable=False)
+    surface_water: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    favorites = relationship('Favorites', backref='planets', lazy=True)
+
+
+class Vehicles(db.Model):
+    __tablename__ = "vehicle"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    model: Mapped[str] = mapped_column(String(100), nullable=False)
+    vehicle_class: Mapped[str] = mapped_column(String(100), nullable=False)
+    manufacturer: Mapped[str] = mapped_column(String(100), nullable=False)
+    length: Mapped[str] = mapped_column(String(100), nullable=False)
+    cost_in_credits: Mapped[str] = mapped_column(String(100), nullable=False)
+    crew: Mapped[str] = mapped_column(String(100), nullable=False)
+    max_atmosphering_speed: Mapped[str] = mapped_column(
+        String(100), nullable=False)
+    cargo_capacity: Mapped[str] = mapped_column(String(100), nullable=False)
+    consumables: Mapped[str] = mapped_column(String(100), nullable=False)
+    url: Mapped[str] = mapped_column(String(200), nullable=False)
+
+    favorites = relationship('Favorites', backref='vehicles', lazy=True)
+
+
+class People(db.Model):
+    __tablename__ = "people"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(100), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    gender: Mapped[str] = mapped_column(String(100), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    height: Mapped[str] = mapped_column(String(20), nullable=False)
+    mass: Mapped[str] = mapped_column(String(40), nullable=False)
+    skin_color: Mapped[str] = mapped_column(String(20), nullable=False)
+    homeworld: Mapped[str] = mapped_column(String(40), nullable=False)
+    url: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    favorites = relationship('Favorites', backref='people', lazy=True)
+
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    people_id: Mapped[int] = mapped_column(
+        ForeignKey('people.id'), nullable=True)
+    vehicle_id: Mapped[int] = mapped_column(
+        ForeignKey('vehicles.id'), nullable=True)
+    planet_id: Mapped[int] = mapped_column(
+        ForeignKey('planets.id'), nullable=True)
